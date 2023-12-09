d58f608e81bdd2b3edc0f8c3c326e0772bbe063f
julioou <54035853+julioou@users.noreply.github.com>
2023-12-09 02:48:35 +0000

    Add leetcode solved questions

---
+++ LeetCode.playground/Contents.swift
@@ -0,0 +1,244 @@
+/*
+ You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+ The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+
+ Example 1:
+
+ Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ Output: [1,2,2,3,5,6]
+ Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ Example 2:
+
+ Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ Output: [1]
+ Explanation: The arrays we are merging are [1] and [].
+ The result of the merge is [1].
+ Example 3:
+
+ Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ Output: [1]
+ Explanation: The arrays we are merging are [] and [1].
+ The result of the merge is [1].
+ Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+ Copyright ©️ 2023 LeetCode All rights reserved
+ */
+
+import Foundation
+
+
+// MARK: - Merge Sorted Array
+
+var t = [1,2,3,0,0,0]
+
+func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    let value = nums1[0..<m] + nums2[0..<n]
+    nums1 = Array(value)
+    nums1.sort()
+}
+
+merge(&t, 3, [2,5,6], 3)
+
+print(t)
+
+
+/*
+ Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+ Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+ Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ Return k.
+ Custom Judge:
+
+ The judge will test your solution with the following code:
+
+ int[] nums = [...]; // Input array
+ int val = ...; // Value to remove
+ int[] expectedNums = [...]; // The expected answer with correct length.
+                             // It is sorted with no values equaling val.
+
+ int k = removeElement(nums, val); // Calls your implementation
+
+ assert k == expectedNums.length;
+ sort(nums, 0, k); // Sort the first k elements of nums
+ for (int i = 0; i < actualLength; i++) {
+     assert nums[i] == expectedNums[i];
+ }
+ If all assertions pass, then your solution will be accepted.
+
+
+
+ Example 1:
+
+ Input: nums = [3,2,2,3], val = 3
+ Output: 2, nums = [2,2,_,_]
+ Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ Example 2:
+
+ Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ Output: 5, nums = [0,1,4,0,3,_,_,_]
+ Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+ Note that the five elements can be returned in any order.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+// MARK: - Remove Element
+
+var n = [0,1,2,2,3,0,4,2]
+
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    nums.removeAll(where: { $0 == val })
+    return 0
+}
+
+
+removeElement(&n, 2)
+
+print(n)
+
+/*
+ Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+ Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+ Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ Return k.
+ Custom Judge:
+
+ The judge will test your solution with the following code:
+
+ int[] nums = [...]; // Input array
+ int[] expectedNums = [...]; // The expected answer with correct length
+
+ int k = removeDuplicates(nums); // Calls your implementation
+
+ assert k == expectedNums.length;
+ for (int i = 0; i < k; i++) {
+     assert nums[i] == expectedNums[i];
+ }
+ If all assertions pass, then your solution will be accepted.
+
+
+
+ Example 1:
+
+ Input: nums = [1,1,2]
+ Output: 2, nums = [1,2,_]
+ Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ Example 2:
+
+ Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+// MARK: - Remove Duplicates from Sorted Array
+
+var dA = [0,0,1,1,1,2,2,3,3,4]
+var ex = [0,1,2,3,4]
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var newArray = Set<Int>()
+    for num in nums {
+        newArray.insert(num)
+    }
+
+    nums = Array(newArray)
+    nums.sort()
+    return newArray.count
+}
+
+removeDuplicates(&dA)
+
+print(dA)
+
+print(dA.count == ex.count)
+
+print("########END")
+/*
+ Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+ Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+ Return k after placing the final result in the first k slots of nums.
+
+ Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+ Custom Judge:
+
+ The judge will test your solution with the following code:
+
+ int[] nums = [...]; // Input array
+ int[] expectedNums = [...]; // The expected answer with correct length
+
+ int k = removeDuplicates(nums); // Calls your implementation
+
+ assert k == expectedNums.length;
+ for (int i = 0; i < k; i++) {
+     assert nums[i] == expectedNums[i];
+ }
+ If all assertions pass, then your solution will be accepted.
+
+
+
+ Example 1:
+
+ Input: nums = [1,1,1,2,2,3]
+ Output: 5, nums = [1,1,2,2,3,_]
+ Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ Example 2:
+
+ Input: nums = [0,0,1,1,1,1,2,3,3]
+ Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+ Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+// MARK: - Remove Duplicates from Sorted Array II
+
+var lv2 = [0,0,1,1,1,1,2,3,3]
+let exLv2 = [0,0,1,1,2,3,3]
+
+func removeDuplicatesMoreLV2(_ nums: inout [Int]) -> Int {
+    var a2 = [Int]()
+    var index = 0
+
+    for element in nums {
+        if index >= 2 {
+            if element != a2[index - 2] {
+                a2.append(element)
+                index += 1
+            }
+            continue
+        } else {
+            a2.append(element)
+            index += 1
+        }
+    }
+
+    nums = a2
+    return index
+}
+
+removeDuplicatesMoreLV2(&lv2)
+
+print(lv2)
+print(lv2.count == exLv2.count)
+
+var lv22 = [1,1,1,2,2,3]
+let exLv22 = [1,1,2,2,3]
+removeDuplicatesMoreLV2(&lv22)
+
+print(lv22)
+print(lv22.count == exLv22.count)
+print("########END")
